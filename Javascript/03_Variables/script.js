08a55ad1183eaa42ac1ddbe0545e08b53ff6d1c7
Pramodjena <pramoodjena@gmail.com>
2025-07-01 23:57:44 +0530

    mern updated

---
+++ Javascript/03_Variables/script.js
@@ -0,0 +1,26 @@
+// console.log(name); // Before Intialization
+
+let name = "Pramod";
+let value = null;
+let age = 25;
+let isMarried = true;
+let mark;
+
+// console.log(score);
+var score;
+
+{
+  let myCard = true; // Block scoped
+}
+// console.log(myCard);
+
+const city = "Cuttack";
+// console.log(city);
+
+let myName = "Pramod";
+let myAge = 26;
+let isMyMarried = false;
+let myCity = "Bhubaneswar";
+
+let userInfo =
+  "My name is" + " " + myName + " " + "and my age is" + " " + myAge;
